94848f328663994e4ef9b1adfcba2ab7bca033cd
Aditya Sharma <aadisharma.in+github@gmail.com>
2025-01-05 15:15:22 +0530

    fix: update Docker Compose configuration for Kafka and rename queue manager to queue worker

--- queue-worker/src/lib/kafka.ts
+++ queue-worker/src/lib/kafka.ts
@@ -1,8 +1,8 @@
-import { Kafka, logLevel, Producer } from "kafkajs";
 import dotenv from "dotenv";
 dotenv.config({
   path: "../.env",
 });
+import { Kafka, logLevel, Producer } from "kafkajs";
 
 let kafka: Kafka;
 let producer: Producer;
@@ -15,14 +15,14 @@ const kafkaProducer = async () => {
     console.log("[kafkajs]: Creating new client...")
     kafka = new Kafka({
       clientId: "analyze-core",
-      brokers: [`${process.env.KAFKA_HOST}:${process.env.KAFKA_PORT}`],
-      logLevel: logLevel.DEBUG
+      brokers: [`${process.env.KAFKA_HOST || "kafka-service"}:${process.env.KAFKA_PORT || 9092}`],
+      logLevel: logLevel.ERROR
     });
     producer = kafka.producer();
     await producer.connect();
 
     return producer;
-  } catch (error) {
+  } catch (error: any) {
     console.error("Error during connect kafka: ", error);
     process.exit(1);
   }
