76b33e046088a8d2a7a78f6802a1e8ea9953728a
Aditya Sharma <aadisharma.in+github@gmail.com>
2025-01-07 12:49:32 +0530

    feat: integrate RabbitMQ consumer and enhance error handling in connection logic

--- queue-worker/src/lib/rabbitmq.ts
+++ queue-worker/src/lib/rabbitmq.ts
@@ -1,39 +1,42 @@
-import amqplib, { Channel, Connection, credentials } from 'amqplib';
-import dotenv from 'dotenv'
+import amqplib, { Channel, Connection, credentials } from "amqplib";
+import dotenv from "dotenv";
 dotenv.config({
-    path: "../.env"
-})
+  path: "../.env",
+});
 
-let connection:Connection;
+let connection: Connection;
 let channel: Channel;
 
-const queue = process.env.RMQ_QUEUE_NAME ?? 'analytics';
+const queue = process.env.RMQ_PRE_ANALYTICS_QUEUE!
 
-const host = process.env.RMQ_HOST!
-const port = process.env.RMQ_PORT!
-const username = process.env.RMQ_USERNAME!
-const password = process.env.RMQ_PASSWORD!
+const host = process.env.RMQ_HOST!;
+const port = process.env.RMQ_PORT!;
+const username = process.env.RMQ_USERNAME!;
+const password = process.env.RMQ_PASSWORD!;
 
-console.log(`amqp://${username}:${password}@${host}:${port}`)
+console.log(`amqp://${username}:${password}@${host}:${port}`);
 
-const connectRmq = async ()=> {
-    if(!connection){
-        console.log("[RMQ]: Creating new connection.")
-        connection = await amqplib.connect(`amqp://${host}:${port}`, {
-            credentials: credentials.plain(username, password)
-        })
+const connectRmq = async () => {
+  try {
+    if (!connection) {
+      console.log("[RMQ]: Creating new connection.");
+      connection = await amqplib.connect(`amqp://${host}:${port}`, {
+        credentials: credentials.plain(username, password),
+      });
     }
-    if(channel) return channel;
-    console.log("[RMQ]: Creating new channel.")
+    if (channel) return channel;
+    console.log("[RMQ]: Creating new channel.");
     channel = await connection.createChannel();
-    
+
     await channel.assertQueue(queue, {
-        durable: true
+      durable: true,
     });
 
     return channel;
-}    
+  } catch (error) {
+    console.error("Failed to connect to rabbit mq:", error);
+    process.exit(1);
+  }
+};
 
-export {
-    connectRmq
-}
+export { connectRmq };
--- queue-worker/src/worker.ts
+++ queue-worker/src/worker.ts
@@ -4,25 +4,43 @@ import { rawAnalytics } from "./db/schema";
 import connectDB from "./db/connect";
 import { kafkaConsumer, kafkaProducer } from "./lib/kafka";
 import { Consumer, Producer } from "kafkajs";
+import { Channel } from "amqplib";
+import { connectRmq } from "./lib/rabbitmq";
 
 let kfConsumer: Consumer;
 let kfProducer: Producer;
+let rmqChannel: Channel;
 
 dotenv.config({
   path: "../.env",
   debug: true,
 });
 
-(async () => {
+const consumeRabbitMq = () => {
+  console.log(
+    `Running rabbitmq consumer for queue ${process.env.RMQ_PRE_ANALYTICS_QUEUE}`
+  );
+  try {
+    rmqChannel.consume(
+      process.env.RMQ_PRE_ANALYTICS_QUEUE!,
+      (message) => {
+        if (message != null) {
+          const msg = message.content.toString();
+          console.log(
+            `Incomming message from ${process.env.RMQ_PRE_ANALYTICS_QUEUE} queue: ${msg}`
+          );
+        }
+      },
+      {
+        noAck: true,
+      }
+    );
+  } catch (error) {}
+};
+
+const consumerKafka = async () => {
+  console.log(`Running kafka consumer for topic `);
   const db = await connectDB();
-  kfConsumer = await kafkaConsumer({
-    groupId: "0",
-    maxWaitTimeInMs: 1024,
-  });
-  kfProducer = await kafkaProducer();
-
-  await kfConsumer.subscribe({ topic: "raw-analytics", fromBeginning: true });
-
   await kfConsumer.run({
     eachBatch: async ({
       batch,
@@ -31,6 +49,12 @@ dotenv.config({
       isRunning,
       isStale,
     }) => {
+
+        if(batch.topic != "raw-analytics"){
+            console.log(`Wrong topic: ${batch.topic}`)
+            return;
+        }
+
       console.log(`Processing batch of ${batch.messages.length} messages`);
 
       const incomingMessages = [];
@@ -44,10 +68,23 @@ dotenv.config({
       }
       await db.insert(rawAnalytics).values(incomingMessages).execute();
       await kfProducer.send({
-        topic: 'event-streams',
-        messages: batch.messages
-      })
+        topic: "event",
+        messages: batch.messages,
+      });
       await heartbeat();
     },
   });
+};
+
+(async () => {
+  rmqChannel = await connectRmq();
+  kfConsumer = await kafkaConsumer({
+    groupId: "0",
+    maxWaitTimeInMs: 1024,
+  });
+  kfProducer = await kafkaProducer();
+
+  await kfConsumer.subscribe({ topic: "raw-analytics", fromBeginning: true });
+
+  await Promise.all([consumerKafka, consumeRabbitMq]);
 })();
