5c5703b7017023ce060c5f5152a3dbd4f53635f6
Aditya Sharma <aadisharma.in+github@gmail.com>
2025-01-08 12:35:48 +0530

    feat: add loading animation component and integrate user authentication handling

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -1,10 +1,22 @@
+"use client"
+import DashLoading from "@/components/dashboard/loading";
 import DashNavbar from "@/components/dashboard/navbar";
+import { useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 
 export default function BlogLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const user = useUser();
+  const navigation = useRouter();
+  if(!user.isLoaded){
+    return <DashLoading/>
+  }
+  if(!user.isSignedIn){
+    return navigation.push('/');
+  }
   return (
     <main className="bg-white overflow-hidden">
       <DashNavbar />
--- frontend/src/components/dashboard/navbar.tsx
+++ frontend/src/components/dashboard/navbar.tsx
@@ -1,3 +1,5 @@
+"use client"
+import { SignedIn, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import React from "react";
 
@@ -30,33 +32,9 @@ export default function DashNavbar() {
           <span>Reporting</span>
           <span>Users</span>
         </div>
-        <button className="border gap-x-2 rounded-r-3xl flex justify-between rounded-l-[100px] px-1 bg-white py-1 shadow-sm text-zinc-800">
-          <div
-            style={{
-              width: 30,
-              height: 30,
-            }}
-          >
-            <Image
-              src="https://avatar.iran.liara.run/public/boy"
-              className="rounded-full"
-              width={30}
-              height={30}
-              alt="Profile"
-            />
-          </div>
-          <div className="flex flex-col">
-            <span className="text-xs">Aditya Sharma</span>
-            <span
-              style={{
-                fontSize: 10,
-              }}
-              className="text-neutral-400"
-            >
-              @aditya_sharma
-            </span>
-          </div>
-        </button>
+        <SignedIn>
+          <UserButton/>
+        </SignedIn>
       </div>
     </nav>
   );
